19ee4d2b7f8c8dd1daa197f706c5197db23c038d
Monu Verma <monucut657@gmail.com>
2026-07-20 08:54:34 +0530

    add  : commit

---
+++ process_incoming.py
@@ -0,0 +1,71 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.metrics.pairwise import cosine_similarity
+from reads_chunks import create_embedding
+import joblib
+import time 
+import requests
+
+
+
+
+def create_embedding(text_list, retries=3):#function for creating the embedings 
+
+    for attempt in range(retries):#retries to create embeddings when model fails to create one ]
+        
+        #creating the embedding using the nomic-emebed for creating the embeddings 
+        try:
+            r = requests.post('http://localhost:11434/api/embed' ,
+                            json={"model" : "nomic-embed-text",
+                                  "input":text_list}, timeout=30)
+
+            if r.status_code != 200:#error handling 
+                print(f"Attempt {attempt+1}: ERROR - {r.json()}")
+                time.sleep(5)
+                continue
+            
+            #creating the json of the embedding produced from the requested embedding
+            embedding = r.json()["embeddings"]
+            return embedding
+        
+        except Exception as e:
+            print(f"Attempt {attempt+1}: Connection failed - {e}")
+            if attempt < retries - 1:
+                time.sleep(5)
+            else:
+                raise Exception(f"Ollama API failed after {retries} attempts: {e}")
+            
+
+
+df  =  joblib.load('embeddings.joblib')
+
+
+
+
+
+
+
+
+
+incoming_query = input('Ask a Question:')
+
+Question_embedding = create_embedding([incoming_query])[0]
+
+
+
+#finding the consine similarity of the question_embedding  with other embeddings 
+ 
+similiarities = cosine_similarity(np.vstack(df['embedding'].values),[Question_embedding]).flatten()#using vstact of numpy for changing the data to 2 dimensional array for making the similarities , flattening for getting in in one column for readability 
+
+
+
+print(similiarities)
+
+top_result = 3
+max_idx = similiarities.argsort()[::-1][0:top_result]
+print(max_idx) #index of the top 3 results of the consine similarity of the quesiton 
+
+
+
+new_df = df.loc[max_idx]
+print(new_df[["Title","number","text"]])
