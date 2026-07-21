6978f817fcc6a079a3944837339304f5ae080870
Monu Verma <monucut657@gmail.com>
2026-07-21 08:40:17 +0530

    add : new version of ollama installed with bge-m3 model

--- process_incoming.py
+++ process_incoming.py
@@ -1,5 +1,4 @@
 import numpy as np 
-import pandas as pd 
 from sklearn.metrics.pairwise import cosine_similarity
 from reads_chunks import create_embedding
 import joblib
@@ -24,23 +23,16 @@ def create_embedding(text_list, ):#function for creating the embedings
 df  =  joblib.load('embeddings.joblib')
 
 incoming_query = input('Ask a Question:')
-
 Question_embedding = create_embedding([incoming_query])[0]
-
-
 
 #finding the consine similarity of the question_embedding  with other embeddings 
  
 similiarities = cosine_similarity(np.vstack(df['embedding'].values),[Question_embedding]).flatten()#using vstact of numpy for changing the data to 2 dimensional array for making the similarities , flattening for getting in in one column for readability 
-
-
-
 print(similiarities)
 
 top_result = 3
 max_idx = similiarities.argsort()[::-1][0:top_result]
 print(max_idx) #index of the top 3 results of the consine similarity of the quesiton 
-
 
 
 new_df = df.loc[max_idx]
