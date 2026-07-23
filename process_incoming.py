a9384d42b2c23b593d750215ca9237d19303b217
Monu Verma <monucut657@gmail.com>
2026-07-23 07:44:27 +0530

    add : prompting for the llm

--- process_incoming.py
+++ process_incoming.py
@@ -3,6 +3,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 from reads_chunks import create_embedding
 import joblib
 from reads_chunks import create_embedding
+import pandas as pd 
 
 
 #saving the data frames in the joblib 
@@ -18,7 +19,7 @@ similiarities = cosine_similarity(np.vstack(df['embedding'].values),[Question_em
 
 
 #top results 
-top_result = 30
+top_result = 5 
 max_idx = similiarities.argsort()[::-1][0:top_result]
 # print(max_idx) #index of the top 3 results of the consine similarity of the quesiton 
 new_df = df.loc[max_idx]
@@ -27,11 +28,24 @@ new_df = df.loc[max_idx]
 
 
 #prompt 
+prompt = f'''I am teaching web development using sigma web development course Here is the video chunks containing. video Title, video number,start time in seconds , end time in seconds, text at that time:
 
 
+{new_df[['Title','number','start','end','text']].to_json()}
+-----------------------------------------
+"{incoming_query}"
+
+User asked this question realted to videos chunks , you have to answer where and how much of the content is tought in which video (in which video and what time stamp) and guide the user to go that particular video
+If user asked any unrelated question , tell him that you can only ask the question only related 
+
+'''
+
+
+with open ('prompt.txt',"w") as f :
+    f.write(prompt)
 #output of the rag system 
-for index , item in new_df.iterrows():
-    print(index,item["Title"],item["number"],item["text"],item["start"],item["end"])
+# for index , item in new_df.iterrows():
+#     print(index,item["Title"],item["number"],item["text"],item["start"],item["end"])
 
 
 
